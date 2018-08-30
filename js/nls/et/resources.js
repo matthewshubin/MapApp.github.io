7d209a1d8fcb5f64f9699d2160aa37137a6fa9b3
Matthew Shubin <mshubin@eecworld.local>
2018-08-30 10:44:36 -0700

    first commit

---
+++ js/nls/et/resources.js
@@ -0,0 +1,16 @@
+define({
+  "map": {
+    "error": "Kaardi loomine ebaõnnestus"
+  },
+  "tools": {
+    "search": {
+      "error": "Asukohta ei leitud",
+      "notWhatYouWanted": "Ei ole see, mida soovisid?",
+      "selectAnother": "Vali muu asukoht",
+      "currentLocation": "Praegune asukoht",
+      "title": "Asukoht"
+    },
+    "legend": "Legendiga kaart",
+    "about": "Info"
+  }
+});
---
+++ js/nls/it/resources.js
@@ -0,0 +1,16 @@
+define({
+  "map": {
+    "error": "Impossibile creare la mappa"
+  },
+  "tools": {
+    "search": {
+      "error": "Posizione non trovata",
+      "notWhatYouWanted": "Non è quanto desiderato?",
+      "selectAnother": "Selezionare un'altra posizione",
+      "currentLocation": "Posizione corrente",
+      "title": "Posizione"
+    },
+    "legend": "Legenda",
+    "about": "Informazioni su"
+  }
+});
